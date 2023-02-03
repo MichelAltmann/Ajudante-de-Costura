6848820646a0983d954fdeee0719ff37fec454af
PedroAugustoMuller <pedro.muller.a@gmail.com>
2023-02-02 21:46:24 -0300

    Atualizei as Classes Domínio

--- src/modelDominio/Cliente.java
+++ src/modelDominio/Cliente.java
@@ -15,13 +15,13 @@ public class Cliente extends Pessoa implements Serializable {
 
     private Costureira costureira;
 
-    public Cliente(Costureira costureira, String cpf, String nome, String email, String telefone, Date dataNascimento, int cep, String estado, String cidade, String rua, int numero) {
-        super(cpf, nome, email, telefone, dataNascimento, cep, estado, cidade, rua, numero);
+    public Cliente(Costureira costureira, String cpf, String nome, String email, String telefone, Date dataNascimento, byte[] imagem, int cep, String estado, String cidade, String rua, int numero) {
+        super(cpf, nome, email, telefone, dataNascimento, imagem, cep, estado, cidade, rua, numero);
         this.costureira = costureira;
     }
 
-    public Cliente(Costureira costureira, int idPessoa, String cpf, String nome, String email, String telefone, Date dataNascimento, int cep, String estado, String cidade, String rua, int numero) {
-        super(idPessoa, cpf, nome, email, telefone, dataNascimento, cep, estado, cidade, rua, numero);
+    public Cliente(Costureira costureira, int idPessoa, String cpf, String nome, String email, String telefone, Date dataNascimento, byte[] imagem, int cep, String estado, String cidade, String rua, int numero) {
+        super(idPessoa, cpf, nome, email, telefone, dataNascimento, imagem, cep, estado, cidade, rua, numero);
         this.costureira = costureira;
     }
 
